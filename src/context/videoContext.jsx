4af00fe43773f6d2156fe68f33b537b97d9cfa65
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-11 08:38:31 +0530

    Added : Video Context

---
+++ src/context/videoContext.jsx
@@ -0,0 +1,35 @@
+import { useContext,createContext,useReducer,useState,useEffect } from "react";
+import { getVideosDataFromServer , getCategoriesDataFromServer } from "../utilities/apis/videosDataApi";
+
+const VideoContext = createContext();
+
+const VideoProvider =({children})=>{
+const [videoData,setVideoData] = useState([])
+const [categoryData,setCategoryData] =  useState([])
+useEffect(()=>{
+    (async () => {
+        const data = await getVideosDataFromServer();
+        setVideoData(data)
+    })() 
+},[])
+useEffect(()=>{
+    (async () => {
+        const data = await getCategoriesDataFromServer();
+        setCategoryData(data)
+    })()
+},[])
+    return(
+        <VideoContext.Provider value=
+        {{ videoData ,
+            setVideoData,
+            categoryData,
+            setCategoryData}
+        }>
+            {children}
+        </VideoContext.Provider>
+    )
+}
+
+const useVideo =()=> useContext(VideoContext);
+
+export {useVideo,VideoProvider}
