e9a293803186a2468d6d5c6eba7f97d442612eab
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-11 08:44:02 +0530

    Added: Video Data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -2,20 +2,127 @@
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
  * */
-
+ import { v4 as uuid } from "uuid";
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: uuid(),
+    title: "Seedhe Midzone Se !",
+    description: "Seedhe Midzone Se ! Straight Outta Madhya Pradesh Written, Performed, Mixed & Mastered by:- ‘Spitfire’ Music:- Missy Elliott's She's A Bitch Artwork:- by ‘Sumit Roy’",
+    creator: "Spitfire",
+    category: "Desi Hip Hop (DHH)",
+    duration: "03:35",
+    views: "306k",
+    uploaded: "4",
+    creatorImage: "https://i.ytimg.com/vi/7pCv8ztVBoI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDSdrnCFO-HQaIUh5Xg8pMVHZTyRg"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: uuid(),
+    title: "PUNCHNAMA",
+    description: "‛DIG A LITTLE DEEPER’ , Pen & Performed By Ayush Khare aka Wordsmth ,Music - A Tale Of Two Citiez By J. Cole, Mixed & Mastered By Spitfire, Recorded At ‘Matchbox Production Studio’, Shot & Edited By ‛Vaibhav Dange’, Directed By Wordsmith",
+    creator: "Wordsmth",
+    category: "Desi Hip Hop (DHH)",
+    duration: "03:05",
+    views: "20k",
+    uploaded: "8",
+    creatorImage: "https://i.ytimg.com/vi/2V6O3lL5eT4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB3XBGq9ltJla-aRM0fAFikeeU0jQ"
+  },
+  {
+    _id: uuid(),
+    title: "AAINA",
+    description: "AAINA, meaning mirror, is a free verse about introspection. The song is inspired by the chaotic nature of online activism - ranging from propaganda to virtue signaling, which contributes to the extreme social polarizations across the world. The lyrics explore personal experiences and vulnerable moments in AFKAP’s life that have made him toxic towards certain types of people. He concludes by stating that it’s extremely convenient to pull people down to feel better about ourselves, but the real conflict is being honest with our introspection. In a lawless world where truth is relative and ambiguous, the only way there can be a meaningful dialogue between opposing views is when people learn how to confront themselves before defying others.",
+    creator: "AFKAP",
+    category: "Desi Hip Hop (DHH)",
+    duration: "03:30",
+    views: "102k",
+    uploaded: "2",
+    creatorImage: "https://i.ytimg.com/vi/xQsHpqDTL-o/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgE4VoHtfoKfogy9TL424CSX-uqg"
+  },
+  {
+    _id: uuid(),
+    title: "GHAZIABAD",
+    description: "CREDITS:Lyrics/Vocals - Ad Vasudev, Music - Control Gang, Mix/Master - Rakht",
+    creator: "Vasudev",
+    category: "Desi Hip Hop (DHH)",
+    duration: "02:08",
+    views: "114k",
+    uploaded: "9",
+    creatorImage: "https://i.ytimg.com/vi/z3q-8U1XHmo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLChGWMF6GNx9doxL65blcFW7bYWKg"
+  },
+  {
+    _id: uuid(),
+    title: "Sanak",
+    description: "What happens when the goddess of death comes calling and the devil inside cannot resist the temptation?,'Sanak' by Authen,Lyrics & Vocals - Authen,Beat By Angellaciencia,Mixed & Mastered by Shhiv @Reset Studios,Label : Reset Music",
+    creator: "Authen",
+    category: "Desi Hip Hop (DHH)",
+    duration: "03:33",
+    views: "4.2M",
+    uploaded: "18",
+    creatorImage: "https://i.ytimg.com/vi/MxTTWO_488M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLByUAumHOramnwd6e1TaAo9HPUinQ"
+  },
+  {
+    _id: uuid(),
+    title: "Mohabbat",
+    description: "In a song about love, Kaam Bhaari bares it all. Written and Performed by: @Kaam Bhaari, Music Producers: RĀKHIS & @Kiss Nuka, Mix and Mastering: Shikhar Yuvraj Manchanda,Label: IncInk Records LLP",
+    creator: "Kaam Bhaari",
+    category: "Desi Hip Hop (DHH)",
+    duration: "03:14",
+    views: "7.6M",
+    uploaded: "21",
+    creatorImage: "https://i.ytimg.com/vi/6xJypoyQFnQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA0H3wni5yp4n16mbN9NgZFiogB_g"
+  },
+  {
+    _id: uuid(),
+    title: "Launda befikar",
+    description: "Everything's alright and if not, it'll be alright. Rebuttal on hope on luck, I believe in hard work! Getting real about the hustle, riches and fame on my music journey, delving into how I found myself running behind sound every day and at the end of the day even with minimal returns in my hand I still believe - Hai Sab Sahi Hai Sab Sahi.Lyricist / Composed / Rap by - Mc Heam, Mixing & Mastering by - Abhijay Sharma, Music by - Bombay Beat Broadcast", 
+    creator: "MC Heam",
+    category: "Lo-Fi Hip Hop (LFH)",
+    duration: "03:03",
+    views: "214k",
+    uploaded: "13",
+    creatorImage: "https://i.ytimg.com/vi/8DMlJ3FVPl8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCTvy0sguWVvl3eWVPoSC67Qnu2Ww"
+  },
+  {
+    _id: uuid(),
+    title: "Sajda",
+    description: "In a song about love, Kaam Bhaari bares it all. Written and Performed by: @Kaam Bhaari, Music Producers: RĀKHIS & @Kiss Nuka, Mix and Mastering: Shikhar Yuvraj Manchanda,Label: IncInk Records LLP",
+    creator: "Ink Heart feat. Hashim Nawaz",
+    category: "Lo-Fi Hip Hop (LFH)",
+    duration: "04:00",
+    views: "697k",
+    uploaded: "11",
+    creatorImage: "https://i.ytimg.com/vi/6ne3oAKKjic/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCuWHKAdUSaCiwO5AGG-eVl9oeiYg"
+  },
+  {
+    _id: uuid(),
+    title: "WINTERFLOW 2.0",
+    description: "Official Music Video of ' WINTER FLOW 2.0 ' performed by SAVAGE, Written/Performed by : SAVAGE, Produced Mix & Master : Z4NE, Directed/Shot by : Humza, Post/Edit by : Humza",
+    creator: "Savage",
+    category: "Desi Hip Hop (DHH)",
+    duration: "02:48",
+    views: "365k",
+    uploaded: "9",
+    creatorImage: "https://i.ytimg.com/vi/msCJO3EppGQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5nbra4JlScofyj9X8GadHeH0j2A"
+  },
+  {
+    _id: uuid(),
+    title: "tu le chal",
+    description: "Sez on the Beat is back with DAKAIT & Aniket Raturi for a brand new song called ‘tu le chal’ ✨ The song has a chill-sound, and was made with the intention of finding some calm in the midst of the constant noise of the external world, especially in the current times.This is the first single from an upcoming EP called 'sabar' which will release later this month. 'sabar' evokes the idea of hanging in there with patience and perseverance, with the hope that good days are just around the corner. THE MVMNT is here!",
+    creator: "DAKAIT x Aniket Raturi",
+    category: "Lo-Fi Hip Hop (LFH)",
+    duration: "04:02",
+    views: "352k",
+    uploaded: "6",
+    creatorImage: "https://i.ytimg.com/vi/n3UVfJTsZ4c/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhsYRTb_bZRcwnXwWijLUWIrPIXw"
+  },
+  {
+    _id: uuid(),
+    title: "Tu Nasha",
+    description: "The first song from KHAYAAL, Tu Nasha is a mellow chillhop track, with catchy vocal melodies and minimalistic music.KHAYAAL is a collection of melody driven beautiful tracks by me. Artist: Dhruv Sthetick, Music & Mix Mastered by : Dhruv Sthetick, Written & Performed by - Dhruv Sthetick, Video & Cover Art : Dhruv Sthetick ",
+    creator: "Dhruv Sthetick",
+    category: "Lo-Fi Hip Hop (LFH)",
+    duration: "03:39",
+    views: "1.2M",
+    uploaded: "23",
+    creatorImage: "https://i.ytimg.com/vi/7sLyZoyOqzM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT33FdJUBDgbVc-U4nBiKHBx_tyA"
   },
 ];
