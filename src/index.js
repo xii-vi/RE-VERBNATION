25f40cee0a940524acb4b390f0fb28a685bd9d6c
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-11 08:49:43 +0530

    Added:  Homepage to the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import { VideoProvider } from "./context/videoContext";
+import { ThemeProvider } from "./context/themeContext";
 const root = createRoot(document.getElementById("root"));
 
 // Call make Server
@@ -11,6 +11,10 @@ makeServer();
 
 root.render(
   <React.StrictMode>
+    <ThemeProvider>
+    <VideoProvider>
     <App />
+    </VideoProvider>
+  </ThemeProvider>
   </React.StrictMode>,
 );
