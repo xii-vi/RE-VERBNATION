84dd81b16a23f36222be2ca215f26e60a9c09c44
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-16 21:15:27 +0530

    Added: Sidebar and VideoPlayer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import { useTheme } from "./context/themeContext"
 import "./style/main.css"
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "./pages/auth/signup";
+import { Sidebar } from "./components/sidebar/sidebar";
+import { SingleVideo } from "./pages/singleVideo/singleVideo";
 function App() {
   const { theme } = useTheme();
   return (
     <div className= {theme === "light" ? "light" : "dark"}>
     <Navbar />
+    <div className="app-layout">
+    <Sidebar />
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/video/:videoId" element={<SingleVideo />} />
     </Routes>
+    </div>
     </div>
   );
 }
