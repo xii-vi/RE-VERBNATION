39c6bbaf6c806ff1c586395808e57a89e7a09d48
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-17 22:31:26 +0530

    feat: Added Liked and History

---
+++ src/pages/history/history.jsx
@@ -0,0 +1,23 @@
+import { useVideo } from "../../context/videoContext"
+import { VideoCard } from "../../components/card/videocard";
+import empty from "../../assest/empty.png"
+import { Link } from "react-router-dom";
+export const History =()=>{
+    const {VideoState:{History}} = useVideo();
+    return(
+    <div className="main">
+    {History.length === 0 ? 
+    <div className="py-4 flex center-flex flex-direction-col">
+        <img className="img-responsive py-2" src={empty} alt="empty-img"/>
+        <Link to="/"><button className="btn btn-primary">Explore</button></Link>
+    </div> :
+        <div className="flex">
+        <div className="flex videos main-display">
+            {History.map((video) => (
+            <VideoCard singleVideoCard={video} key={video._id}/>
+            ))}
+            </div>
+    </div>}
+    </div>
+    )
+}
